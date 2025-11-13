eb918f28d8710b8bcfff86b27927442a10224a20
João Guilherme Da Silva Brito <164958812+GuiBriito@users.noreply.github.com>
2025-11-13 11:59:43 -0300

    Add files via upload

---
+++ Unicid/Algoritimo e pensamento computacional/Linguagem_C/while.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<conio.h>
+#include<math.h>
+
+int main()
+{
+    int x;
+    x=1;
+    while(x<=1000)
+    {printf("%d", x);
+        x= x + 1;
+    }
+    printf("\n");
+    system("PAUSE");
+    return 0;
+}
